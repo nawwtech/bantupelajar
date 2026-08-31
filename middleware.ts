d8fac1140622b305d84499c5fa372b84a7831b5b
nawwtech <blavournaww@gmail.com>
2026-09-01 05:50:48 +0700

    naw

---
+++ middleware.ts
@@ -0,0 +1,3 @@
+import {NextResponse,type NextRequest} from 'next/server';import {createServerClient} from '@supabase/ssr';import {SUPABASE_URL,SUPABASE_PUBLISHABLE_KEY} from './app/lib/config';
+export async function middleware(request:NextRequest){let response=NextResponse.next({request});const supabase=createServerClient(SUPABASE_URL,SUPABASE_PUBLISHABLE_KEY,{cookies:{getAll(){return request.cookies.getAll()},setAll(items){items.forEach(({name,value,options})=>{request.cookies.set(name,value);response.cookies.set(name,value,options)})}}});await supabase.auth.getUser();return response}
+export const config={matcher:['/((?!_next/static|_next/image|favicon.ico).*)']};
